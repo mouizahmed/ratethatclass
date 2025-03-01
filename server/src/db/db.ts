5763c6eac808ae5e85900f3f720f91c4eaba602d
Mouiz Ahmed <mouiza@my.yorku.ca>
2025-02-28 20:58:25 -0500

    x

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,16 +1,14 @@
 import pg from 'pg';
 const { Pool } = pg;
-import * as dotenv from 'dotenv';
-const config = dotenv.config().parsed;
 
 const pool = new Pool({
-  host: config.DB_HOST,
-  user: config.DB_USER,
-  password: config.DB_PASS,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
-  database: config.DB_NAME,
+  database: process.env.DB_NAME,
 });
 
 export { pool };
