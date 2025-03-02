386605e713c8ecedd8d7798f1f0e2ed0dac05e96
Mouiz Ahmed <mouiza@my.yorku.ca>
2025-03-01 22:13:15 -0500

    x

--- server/firebase/firebase.ts
+++ server/firebase/firebase.ts
@@ -1,11 +1,26 @@
 import admin, { ServiceAccount } from 'firebase-admin';
 import { getAuth } from 'firebase-admin/auth';
 
+// Log environment variables for debugging
+console.log('Environment Variables:', {
+  TYPE: process.env.TYPE,
+  PROJECT_ID: process.env.PROJECT_ID,
+  PRIVATE_KEY_ID: process.env.PRIVATE_KEY_ID,
+  PRIVATE_KEY: process.env.PRIVATE_KEY ? 'Exists' : 'Not Set',
+  CLIENT_EMAIL: process.env.CLIENT_EMAIL,
+  CLIENT_ID: process.env.CLIENT_ID,
+  AUTH_URI: process.env.AUTH_URI,
+  TOKEN_URI: process.env.TOKEN_URI,
+  AUTH_PROVIDER_X509_CERT_URL: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  CLIENT_X509_CERT_URL: process.env.CLIENT_X509_CERT_URL,
+  UNIVERSE_DOMAIN: process.env.UNIVERSE_DOMAIN,
+});
+
 const serviceAccount = {
   type: process.env.TYPE,
   project_id: process.env.PROJECT_ID,
   private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY,
+  private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
   client_email: process.env.CLIENT_EMAIL,
   client_id: process.env.CLIENT_ID,
   auth_uri: process.env.AUTH_URI,
