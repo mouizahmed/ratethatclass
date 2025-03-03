46208ec52df0134dba9bf4b41d4a541323720fe3
Mouiz Ahmed <mouiza@my.yorku.ca>
2025-03-03 13:41:55 -0500

    x

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Search from '@/components/common/Search';
 import { getUniversities } from '@/requests/getRequests';
 import { University } from '@/types/university';
@@ -13,7 +13,6 @@ import { useAlert } from '@/contexts/alertContext';
 
 export default function Home() {
   const [universityList, setUniversityList] = useState<University[]>([]);
-  const [chunkedUniversityList, setChunkedUniversityList] = useState<University[][]>([]);
   const [searchValue, setSearchValue] = useState<string>('');
   const [selectedValue, setSelectedValue] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
@@ -25,8 +24,6 @@ export default function Home() {
         setLoading(true);
         const universities = await getUniversities();
         setUniversityList(universities);
-        const chunkedList = chunk(universities);
-        setChunkedUniversityList(chunkedList);
         setLoading(false);
       } catch (error) {
         console.log(error);
@@ -39,21 +36,35 @@ export default function Home() {
   }, [addAlert]);
 
   const chunk = (list: University[]) => {
-    const chunks = [];
+    const chunks: University[][] = [];
     let size = 3;
     if (window.innerWidth > 788) size = 6;
     for (let i = 0; i < list.length; i += size) {
       chunks.push(list.slice(i, i + size));
     }
-
     return chunks;
   };
 
+  const filterSearch = useCallback(
+    (university: University) => {
+      if (searchValue.length === 0) return true;
+      return university.university_name.toLowerCase().includes(searchValue.toLowerCase());
+    },
+    [searchValue]
+  );
+
+  const filteredUniversities = React.useMemo(() => universityList.filter(filterSearch), [universityList, filterSearch]);
+
+  const chunkedUniversities = React.useMemo(() => chunk(filteredUniversities), [filteredUniversities]);
+
   return (
     <div className="flex flex-col items-center gap-10 p-8 sm:p-20">
-      <h1 className="scroll-m-20 text-2xl lg:text-3xl font-bold tracking-tight lg:text-5xl">
-        Course selection made easy
-      </h1>
+      <div className="flex flex-col items-center gap-3">
+        <h1 className="scroll-m-20 text-2xl lg:text-3xl font-bold tracking-tight lg:text-5xl">
+          Course selection made easy
+        </h1>
+        <p className="leading-7">Write anonymous reviews about your classes and help others make informed decisions.</p>
+      </div>
       <div className="w-full max-w-3xl">
         <Search
           data={universityList}
@@ -79,7 +90,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center w-full">
         {loading ? (
           <Spinner size="medium" />
-        ) : chunkedUniversityList.length != 0 ? (
+        ) : chunkedUniversities.length !== 0 ? (
           <Carousel
             className="w-full max-w-3xl h-[500px]"
             opts={{
@@ -93,35 +104,33 @@ export default function Home() {
             ]}
           >
             <CarouselContent>
-              {chunkedUniversityList &&
-                chunkedUniversityList.map((chunk, pageIndex) => (
-                  <CarouselItem key={pageIndex}>
-                    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                      {chunk.map((item, index) => (
-                        <Link key={index} href={`/${item.university_name.replaceAll(' ', '_').toLowerCase()}`}>
-                          <Card className="w-50 h-50 pb-5 hover:bg-zinc-100 cursor-pointer hover:shadow-xl hover:border-zinc-300">
-                            <CardContent className="flex items-center justify-center p-4">
-                              <Image
-                                src={item.university_logo}
-                                alt={item.university_name}
-                                width={150}
-                                height={150}
-                                className="w-36 h-36 object-contain"
-                              />
-                            </CardContent>
-                            <CardFooter className="flex flex-col justify-between items-center text-center h-[70px] p-1">
-                              {item.university_name}
-                              <p className="scroll-m-20 text-md font-semibold tracking-tight">
-                                {item.review_num}
-                                {' reviews'}
-                              </p>
-                            </CardFooter>
-                          </Card>
-                        </Link>
-                      ))}
-                    </div>
-                  </CarouselItem>
-                ))}
+              {chunkedUniversities.map((chunk, pageIndex) => (
+                <CarouselItem key={pageIndex}>
+                  <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {chunk.map((item, index) => (
+                      <Link key={index} href={`/${item.university_name.replaceAll(' ', '_').toLowerCase()}`}>
+                        <Card className="w-50 h-50 pb-5 hover:bg-zinc-100 cursor-pointer hover:shadow-xl hover:border-zinc-300">
+                          <CardContent className="flex items-center justify-center p-4">
+                            <Image
+                              src={item.university_logo}
+                              alt={item.university_name}
+                              width={150}
+                              height={150}
+                              className="w-36 h-36 object-contain"
+                            />
+                          </CardContent>
+                          <CardFooter className="flex flex-col justify-between items-center text-center h-[70px] p-1">
+                            {item.university_name}
+                            <p className="scroll-m-20 text-md font-semibold tracking-tight">
+                              {item.review_num} reviews
+                            </p>
+                          </CardFooter>
+                        </Card>
+                      </Link>
+                    ))}
+                  </div>
+                </CarouselItem>
+              ))}
             </CarouselContent>
             <CarouselPrevious className="m-5 lg:m-0" />
             <CarouselNext className="m-5 lg:m-0" />
