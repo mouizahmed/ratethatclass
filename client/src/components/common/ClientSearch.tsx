fd8aaff44746ae77dca1401abe42b19569fcf7e1
Mouiz Ahmed <mouiza@my.yorku.ca>
2025-05-22 17:10:49 -0400

    build fix

--- client/src/components/common/ClientSearch.tsx
+++ client/src/components/common/ClientSearch.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import Search from './Search';
 import { University } from '@/types/university';
-import { Input } from '@/components/ui/input';
+// import { Input } from '@/components/ui/input';
 import { Search as SearchIcon } from 'lucide-react';
 
 interface ClientSearchProps {
